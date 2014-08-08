204386eb531a3e1361faaa6aa843b45002f6cc3e
guango <UG@uris-mbp.privatebox>
2014-08-08 13:13:19 +0300

    Panned Rectangle

--- TaskTSD/TaskTSD/FirstViewController.m
+++ TaskTSD/TaskTSD/FirstViewController.m
@@ -15,6 +15,9 @@
 
 @implementation FirstViewController
 
+float origX;
+float origY;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -22,7 +25,10 @@
 
 	BubbleView * bubbleView = [[BubbleView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
 	bubbleView.backgroundColor = [UIColor clearColor];
+    UIPanGestureRecognizer *bubblePanGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(moveBubble:)];
 
+    [bubbleView addGestureRecognizer:bubblePanGesture];
+    
 	[self.view addSubview:bubbleView];
 	
 }
@@ -31,6 +37,21 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)moveBubble:(UIPanGestureRecognizer *)pan
+{
+    if([pan state] == UIGestureRecognizerStateBegan){
+        origX = pan.view.center.x;
+        origY = pan.view.center.y;
+    }
+    
+    CGPoint newCenter;
+    newCenter.x = origX + [pan translationInView:pan.view].x;
+    newCenter.y = origY + [pan translationInView:pan.view].y;
+    pan.view.center = newCenter;
+    
+    //NSLog(@"Panned %f, %f", [pan translationInView:pan.view].x, [pan translationInView:pan.view].y );
 }
 
 @end
