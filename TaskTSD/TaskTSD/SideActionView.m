b7c987f5b0e852973cb388a6c1c376a5f6bca7c5
Ziv Levy <levi.ziv@gmail.com>
2015-06-01 14:49:28 +0300

    add action icons

--- TaskTSD/TaskTSD/SideActionView.m
+++ TaskTSD/TaskTSD/SideActionView.m
@@ -11,6 +11,7 @@
 
 #define k_SideActionWidth 30
 #define k_TabBarHeight 49
+#define kActionIconSize 20
 
 @implementation SideActionView
 
@@ -21,36 +22,43 @@
     CGFloat y = 0;
     CGFloat screenHeight = [[UIScreen mainScreen] bounds].size.height;
     CGFloat screenWidth = [[UIScreen mainScreen] bounds].size.width;
-    
-    switch(type){
+    UIImage *icon;
+
+    switch (type) {
         case Done: {
             y = (screenHeight - k_TabBarHeight) / 2;
+            icon = [UIImage imageNamed:@"complete"];
         }
             break;
         case Delegate: {
-            
+            icon = [UIImage imageNamed:@"share"];
         }
             break;
-        case Delete:{
+        case Delete: {
             y = (screenHeight - k_TabBarHeight) / 2;
             x = screenWidth - k_SideActionWidth;
-            
+            icon = [UIImage imageNamed:@"close"];
         }
             break;
-        case Schedule:{
+        case Schedule: {
             x = screenWidth - k_SideActionWidth;
-            
-        }break;
+            icon = [UIImage imageNamed:@"schedule"];
+        }
+            break;
         default:
+            NSLog(@"ERROR with type");
             break;
     }
-    
-    CGRect rect = CGRectMake(x, y, k_SideActionWidth, ((screenHeight - k_TabBarHeight) / 2) -2);
+
+    CGRect rect = CGRectMake(x, y, k_SideActionWidth, ((screenHeight - k_TabBarHeight) / 2) - 2);
     self = [super initWithFrame:rect];
     if (self) {
         self.actionType = type;
         self.backgroundColor = [UIColor colorWithRed:0.5f green:0.5f blue:0.5f alpha:1.0];
         self.alpha = kSideBarDefaultOpacity;
+        UIImageView *iconView = [[UIImageView alloc] initWithFrame:CGRectMake((k_SideActionWidth - kActionIconSize) / 2, (rect.size.height / 2) - (kActionIconSize / 2), kActionIconSize, kActionIconSize)];
+        iconView.image = icon;
+        [self addSubview:iconView];
     }
     return self;
 }
